4f6a9095b14ff408ca18cf2a4cccb94dcd54e0d8
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-25 20:43:32 -0400

    commit tree 5859bed7496894f9bdfb8f61c9b0599045bac255

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let dist = distance - 400; 
+    if (distance <= 400) {
+        return 0;
+    }   else if (distance > 400 && distance < 2000) {
+        return dist * 0.02;
+    }   else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }   else {
+        return 'cannot travel that far'
+    }
+}
